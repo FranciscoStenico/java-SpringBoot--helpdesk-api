604b45a82faf286c02d06a35447fcdda466b3290
Francisco Stenico <francisco.stenico@gmail.com>
2023-01-24 19:54:07 -0300

    FEAT: Development configuration + MySQL connection

--- src/main/java/com/api/helpdesk/config/DevConfig.java
+++ src/main/java/com/api/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.api.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,15 +9,22 @@ import org.springframework.context.annotation.Profile;
 import com.api.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String propertiesKey;
+
     @Bean
-    public void setTables() {
-        this.dbService.setTables();
+    public boolean setTables() {
+        if (propertiesKey.equals("create")) {
+            this.dbService.setTables();
+        }
+
+        return false;
     }
 
 }
--- src/main/java/com/api/helpdesk/services/DBService.java
+++ src/main/java/com/api/helpdesk/services/DBService.java
@@ -18,28 +18,28 @@ import com.api.helpdesk.repositories.TechnicianRepository;
 @Service
 public class DBService {
 
-        @Autowired
-        private TechnicianRepository techRepo;
-        @Autowired
-        private ClientRepository clientRepo;
-        @Autowired
-        private CallRepository callRepo;
+  @Autowired
+  private TechnicianRepository techRepo;
+  @Autowired
+  private ClientRepository clientRepo;
+  @Autowired
+  private CallRepository callRepo;
 
-        public void setInstances() {
-                Technician technician1 = new Technician(
-                                null, "Francisco Stenico", "12345678900", "francisco@teste.com", "1234Teste!");
-                technician1.addProfile(Profile.ADMIN);
+  public void setTables() {
+    Technician technician1 = new Technician(
+        null, "Francisco Stenico", "12345678900", "francisco@teste.com", "1234Teste!");
+    technician1.addProfile(Profile.ADMIN);
 
-                Client client1 = new Client(
-                                null, "Carolina Maronese", "98765432100", "carolina@teste.com", "1234Teste!");
-                client1.addProfile(Profile.CLIENT);
+    Client client1 = new Client(
+        null, "Carolina Maronese", "98765432100", "carolina@teste.com", "1234Teste!");
+    client1.addProfile(Profile.CLIENT);
 
-                Call call1 = new Call(null, Priority.MEDIUM_PRIORITY, Status.IN_PROGRESS, "Chamado - 01",
-                                "Observações sobre o chamado", technician1, client1);
+    Call call1 = new Call(null, Priority.MEDIUM_PRIORITY, Status.IN_PROGRESS, "Chamado - 01",
+        "Observações sobre o chamado", technician1, client1);
 
-                techRepo.saveAll(Arrays.asList(technician1));
-                clientRepo.saveAll(Arrays.asList(client1));
-                callRepo.saveAll(Arrays.asList(call1));
-        }
+    techRepo.saveAll(Arrays.asList(technician1));
+    clientRepo.saveAll(Arrays.asList(client1));
+    callRepo.saveAll(Arrays.asList(call1));
+  }
 
 }
